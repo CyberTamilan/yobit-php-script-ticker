5738f0d07ee3446b684cb312653d1f4fe21b420f
CyberKing <33313629+CyberTamilan@users.noreply.github.com>
2018-02-04 13:00:52 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,108 +3,121 @@ header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
 header("Cache-Control: post-check=0, pre-check=0", false);
 header("Pragma: no-cache");
 
-
-
-
-$url = "https://yobit.net/api/3/ticker/doge_usd";
-$json = file_get_contents($url);
-$json_data = json_decode($json, true);
-$dogesusd = $json_data["doge_usd"]["sell"];
-
-echo "<table border=1>";
-echo "<th>DOGE/USD</th>";
-
-echo "<td>$";
-echo $dogesusd;
-echo "</td></table>"; 
-
-
-
-$url = "https://yobit.net/api/3/ticker/btc_usd";
+	$currencys = array("trx", "xvg", "linda", "token", "ltc"); 
+	
+	$url = "https://yobit.net/api/3/ticker/btc_usd";
 $json = file_get_contents($url);
 $json_data = json_decode($json, true);
 $btcusd = $json_data["btc_usd"]["sell"];
 
 
-$url = "https://yobit.net/api/3/ticker/doge_btc";
-$json = file_get_contents($url);
-$json_data = json_decode($json, true);
-$dogebtc=$json_data["doge_btc"]["sell"];
-$dogebtcs=number_format($dogebtc, 8);
-
-
-echo "<table border=1>";
-echo "<th>DOGE/BTC</th>";
-echo "<td>$dogebtcs ";
-echo "($";
-echo $json_data["doge_btc"]["sell"] * $btcusd;
-echo ")</td></table>";
-
-$url = "https://yobit.net/api/3/ticker/eth_usd";
+	$url = "https://yobit.net/api/3/ticker/eth_usd";
 $json = file_get_contents($url);
 $json_data = json_decode($json, true);
 $ethusd = $json_data["eth_usd"]["sell"];
-
-
-$url = "https://yobit.net/api/3/ticker/doge_eth";
-$json = file_get_contents($url);
-$json_data = json_decode($json, true);
-$dogeeth=$json_data["doge_eth"]["sell"];
-$dogeeths=number_format($dogeeth, 8);
-echo "<table border=1>";
-echo "<th>DOGE/ETH</th>";
-
-echo "<td>$dogeeths";
-
-echo "($";
-echo  number_format($json_data["doge_eth"]["sell"] * $ethusd, 8);
-echo ")</td></table>"; 
-
-
-
-
-$url = "https://yobit.net/api/3/ticker/waves_usd";
+	
+	$url = "https://yobit.net/api/3/ticker/waves_usd";
 $json = file_get_contents($url);
 $json_data = json_decode($json, true);
 $wavesusd = $json_data["waves_usd"]["sell"];
 
-
-$url = "https://yobit.net/api/3/ticker/doge_waves";
+$url = "https://yobit.net/api/3/ticker/doge_usd";
 $json = file_get_contents($url);
 $json_data = json_decode($json, true);
-$dogewaves=$json_data["doge_waves"]["sell"];
-$dogewavess=number_format($dogewaves, 8);
+$dogeusd = $json_data["doge_usd"]["sell"];
+
+	
+	foreach ($currencys as $currency)
+	{
+	
+	
+	$url = "https://yobit.net/api/3/ticker/{$currency}_usd" ;
+$json = file_get_contents($url);
+$json_data = json_decode($json, true);
+$cursusd = $currency;
+	$cursusd = $json_data["{$currency}_usd"]["sell"];
+
 echo "<table border=1>";
-echo "<th>DOGE/WAVES</th>";
+echo "<th>$currency/USD</th>";
 
-echo "<td>$dogewavess";
+echo "<td>$";
+echo number_format($cursusd, 8);
+echo "</td></table>";  
+        
+    
+   
+        $url = "https://yobit.net/api/3/ticker/{$currency}_btc" ;
+$json = file_get_contents($url);
+$json_data = json_decode($json, true);
+$cursusd = $currency;
+	$cursusd = $json_data["{$currency}_btc"]["sell"];
 
+echo "<table border=1>";
+echo "<th>$currency/BTC</th>";
+
+echo "<td>"; 
+echo number_format($cursusd, 8) ;
 echo "($";
-echo  number_format($json_data["doge_waves"]["sell"] * $wavesusd, 8);
-echo ")</td></table>"; 
+echo number_format($cursusd, 8) * $btcusd ;
+echo ")";
+echo "</td></table>";  
+        
+    
+   
+        $url = "https://yobit.net/api/3/ticker/{$currency}_eth" ;
+$json = file_get_contents($url);
+$json_data = json_decode($json, true);
+$cursusd = $currency;
+	$cursusd = $json_data["{$currency}_eth"]["sell"];
+
+echo "<table border=1>";
+echo "<th>$currency/ETH</th>";
+
+echo "<td>";
+echo number_format($cursusd, 8);
+echo "($";
+echo number_format($cursusd, 8) * $ethusd ;
+echo ")";
+echo "</td></table>";  
+       
+ 
+   
+        $url = "https://yobit.net/api/3/ticker/{$currency}_doge" ;
+$json = file_get_contents($url);
+$json_data = json_decode($json, true);
+$cursusd = $currency;
+	$cursusd = $json_data["{$currency}_doge"]["sell"];
+
+echo "<table border=1>";
+echo "<th>$currency/DOGE</th>";
+
+echo "<td>";
+echo number_format($cursusd, 8);
+echo "($";
+echo number_format($cursusd, 8) * $dogeusd ;
+echo ")";
+echo "</td></table>";  
+        
+    
+    
+        $url = "https://yobit.net/api/3/ticker/{$currency}_waves" ;
+$json = file_get_contents($url);
+$json_data = json_decode($json, true);
+$cursusd = $currency;
+	$cursusd = $json_data["{$currency}_waves"]["sell"];
+
+echo "<table border=1>";
+echo "<th>$currency/WAVES</th>";
+
+echo "<td>";
+echo number_format($cursusd, 8);
+echo "($";
+echo number_format($cursusd, 8) * $wavesusd ;
+echo ")";
+echo "</td></table>";  
+       
+    
+	
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+}
